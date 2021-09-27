1a84cb0264478c194f7a929ae486a8241211a281
ccocci <carla.coccia@hispeed.ch>
2021-09-27 08:44:51 +0200

    get general school and class info

--- venv/scrape_mined.py
+++ venv/scrape_mined.py
@@ -4,11 +4,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 import time
 import csv
+import pandas as pd
 
 
-
-# san_miguel="12"
-deps=["12"]
 
 browser=webdriver.Chrome()
 
@@ -28,6 +26,10 @@ def get_school_links():
     browser.implicitly_wait(10)
     links = [elem.get_attribute('href') for elem in elems]
     return links
+
+sel_field= browser.find_element_by_xpath("//*[@id='form:b_departamento']")
+#deps= [str(x) for x in range(1,15)]
+deps=["12"]
 
 # go through all departments
 for dep in deps:
@@ -55,3 +57,55 @@ for dep in deps:
         links.extend(page_links)
 
     school_links.extend(links)
+
+
+
+rows_list = []
+
+## loop over each school!
+
+data_rows=[]
+
+
+test_link="https://portal.siges.sv/pp/sede?sede=2083"
+browser.get(test_link)
+browser.implicitly_wait(10)
+
+dict={}
+
+# school info
+dict["code"]=browser.find_element_by_xpath('//*[@id="form:input_codigo"]').text
+dict["website"]=browser.find_element_by_xpath('//*[@id="form:j_idt31"]/div[3]/a').get_attribute("href")
+dict["name"]=browser.find_element_by_xpath('//*[@id="form:input_nombre"]').text
+dict["type"]=browser.find_element_by_xpath('//*[@id="form:output_tipo_sede"]').text
+dict["address"]=browser.find_element_by_xpath('//*[@id="form:j_idt31"]/div[5]').text
+dict["tel"]=browser.find_element_by_xpath('//*[@id="form:input_telefono"]').text
+dict["shift"]=browser.find_element_by_xpath('//*[@id="form:opt_jornada"]').text
+dict["email"]=browser.find_element_by_xpath('//*[@id="form:j_idt31"]/div[10]/a').text
+
+sel_grade=Select(browser.find_element_by_xpath("//*[@id='form:b_nivel']"))
+sel_grade.select_by_visible_text("Educación Básica")
+search = browser.find_element_by_xpath('//*[@id="form:btnBuscar"]')
+search.click()
+
+
+tbl = browser.find_element_by_xpath('//*[@id="form:basicDT"]/div/table').get_attribute('outerHTML')
+df  = pd.DataFrame(pd.read_html(tbl)[0])
+# select rows we need
+df=df[["Nombre grado","2019","2020","2021"]]
+# cut the first 4 number b/c they are repetition of year
+df[["2019","2020","2021"]]= df[["2019","2020","2021"]].applymap(lambda x: int(str(x)[4:]))
+
+grades = {"Primer": "1","Segundo": "2","Tercer": "3","Quarto": "4", "Quinto": "5","Sexto": "6"}
+
+for grade in grades:
+    students=df[df["Nombre grado"]==("Nombre grado"+grade+" Grado")].loc[:,["2019","2020","2021"]]
+    students=list(students.values.flatten())
+    grade_number=grades.get(grade)
+    var_name="students_grade"+grade_number
+    dict[var_name]=students
+
+
+#data_rows.append(dict)
+
+##data = pd.DataFrame(data_rows)  --> if not working add attribute columns=['A','B','C','D','E']
